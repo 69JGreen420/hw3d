3f7d896dfcb12b8f230d73bb7b16c46d0971d117
JGreen <JoshGeeeeee@outlook.com>
2026-09-14 09:20:50 +0100

    2 Triangles

--- hw3d/Graphics.cpp
+++ hw3d/Graphics.cpp
@@ -118,6 +118,11 @@ void Graphics::DrawTestTriangle()
 		{ 0.0f,0.5f },
 		{ 0.5f,-0.5f },
 		{ -0.5f,-0.5f },
+
+		// Triangle must have anti-clockwise organised vertices
+		{ 0.5f,1.0f },
+		{ 1.0f,0.5f },
+		{ 0.5f,0.5f }
 	};
 	wrl::ComPtr<ID3D11Buffer> pVertexBuffer;
 	D3D11_BUFFER_DESC bd = {};
