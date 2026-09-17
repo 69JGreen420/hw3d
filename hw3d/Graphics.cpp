40f618a41cb87c97d7e17c0c77e40587199d2281
JGreen <JoshGeeeeee@outlook.com>
2026-09-17 13:07:13 +0100

    Messing about with matrices

--- hw3d/Graphics.cpp
+++ hw3d/Graphics.cpp
@@ -106,7 +106,7 @@ void Graphics::ClearBuffer( float red,float green,float blue ) noexcept
 void Graphics::DrawTestTriangle(float angle, float x, float y)
 {
 	dx::XMVECTOR v = dx::XMVectorSet(3.0f, 3.0f, 0.0f, 0.0f);
-	auto result = dx::XMVector4Dot(v, v); // Multiply by itself
+	auto result = dx::XMVector3Transform(v, dx::XMMatrixScaling(1.5f, 0.0f, 0.0f));
 	auto X = dx::XMVectorGetX(result);
 
 	HRESULT hr;
